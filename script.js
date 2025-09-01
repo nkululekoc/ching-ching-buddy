8674d7ac22ccc0e1d40df5168aab4f7ad8f0276c
nkululekoc <155824459+nkululekoc@users.noreply.github.com>
2025-09-01 22:00:54 +0200

    Added FAQs to the app

--- script.js
+++ script.js
@@ -61,3 +61,34 @@ document.addEventListener("DOMContentLoaded", function () {
 
   calculateLoan();
 });
+
+(function () {
+  if (!window.chatbase || window.chatbase("getState") !== "initialized") {
+    window.chatbase = (...arguments) => {
+      if (!window.chatbase.q) {
+        window.chatbase.q = [];
+      }
+      window.chatbase.q.push(arguments);
+    };
+    window.chatbase = new Proxy(window.chatbase, {
+      get(target, prop) {
+        if (prop === "q") {
+          return target.q;
+        }
+        return (...args) => target(prop, ...args);
+      },
+    });
+  }
+  const onLoad = function () {
+    const script = document.createElement("script");
+    script.src = "https://www.chatbase.co/embed.min.js";
+    script.id = "IF0-m3C2uD3AZM3p39JB7";
+    script.domain = "www.chatbase.co";
+    document.body.appendChild(script);
+  };
+  if (document.readyState === "complete") {
+    onLoad();
+  } else {
+    window.addEventListener("load", onLoad);
+  }
+})();
